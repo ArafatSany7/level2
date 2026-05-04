94340db1805f8e528a6c437487f992695343afda
ArafatSany7 <human.sany7@gmail.com>
2026-05-05 03:01:17 +0600

    inheritance

---
+++ TypeScript-Learning/src/inheritance.ts
@@ -0,0 +1,37 @@
+class Parent {
+  constructor(
+    public name: string,
+    public age: number,
+    public address: string,
+  ) {}
+
+  //   COMMON METHOD
+  getSleep(numberOfHour: number) {
+    console.log(`${this.name} Banda ${numberOfHour} ghumai`);
+  }
+}
+
+class Student extends Parent {}
+
+class Teacher extends Parent {
+  constructor(
+    public name: string,
+    public age: number,
+    public address: string,
+    public designation: string,
+  ) {
+    super(name, age, address);
+  }
+
+  takeClass(numberOfClass: number) {
+    console.log(`${this.name} ${numberOfClass} ghonta class nei`);
+  }
+}
+
+const student1 = new Student("Modu", 14, "BD");
+student1.getSleep(12);
+
+const teacher1 = new Teacher("Jodu", 45, "Deshi", "lecturer");
+
+console.log(teacher1.address);
+teacher1.takeClass(5);
