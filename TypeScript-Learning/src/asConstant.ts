5c5ad24258b7738c0531a4e68b71baf7c5c420a6
ArafatSany7 <human.sany7@gmail.com>
2026-05-04 18:52:55 +0600

    enum

---
+++ TypeScript-Learning/src/asConstant.ts
@@ -0,0 +1,13 @@
+// as constant assertion
+
+// enum userRoles{
+//     Admin= "Admin",
+//     Editor = "Editor",
+//     Viewer = "Viewer"
+// }
+
+const userRols = {
+  Admin: "Admin",
+  Editor: "Editor",
+  Viewer: "Viewer",
+} as const;
