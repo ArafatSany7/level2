912501b5775565969e5d55309294ffa865769c89
ArafatSany7 <human.sany7@gmail.com>
2026-05-05 02:40:09 +0600

    class

---
+++ TypeScript-Learning/src/class.ts
@@ -0,0 +1,45 @@
+// OPP - CLASS - OBJECT
+
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
+
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+
+//   makesound() {
+//     console.log(`${this.name} is making sound ${this.sound}`);
+//   }
+// }
+
+// PARAMETER PROPERTIES
+class Animal {
+  //   name: string;
+  //   species: string;
+  //   sound: string;
+
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string,
+  ) {
+    // this.name = name;
+    // this.species = species;
+    // this.sound = sound;
+  }
+
+  makesound() {
+    console.log(`${this.name} is making sound ${this.sound}`);
+  }
+}
+
+const dog = new Animal("Dogesh", "Dog", "gheo gheo");
+const cat = new Animal("Billu", "Bilai", "mew mew");
+
+console.log(dog.name);
+console.log(cat.sound);
+cat.makesound();
