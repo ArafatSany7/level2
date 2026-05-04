3e41b6c2ca1c9d90c600ebe4cd1c456f3cf2b159
ArafatSany7 <human.sany7@gmail.com>
2026-05-04 19:01:08 +0600

    asConsts

--- TypeScript-Learning/src/asConstant.ts
+++ TypeScript-Learning/src/asConstant.ts
@@ -6,8 +6,17 @@
 //     Viewer = "Viewer"
 // }
 
-const userRols = {
-  Admin: "Admin",
-  Editor: "Editor",
-  Viewer: "Viewer",
+const userRoles = {
+  Admin: "ADMIN",
+  Editor: "EDITOR",
+  Viewer: "VIEWER",
 } as const;
+
+const canEdit = (role: (typeof userRoles)[keyof typeof userRoles]) => {
+  if (role === userRoles.Admin || role === userRoles.Editor) {
+    return true;
+  } else return false;
+};
+
+const isEditPermission = canEdit(userRoles.Admin);
+console.log(isEditPermission);
