a530bf24839742dba76e20e6cb4c99e916913d6c
ArafatSany7 <human.sany7@gmail.com>
2026-05-10 22:04:52 +0600

    Route Handling with HTTP Requests & Responses

--- Node-Learning/src/server.ts
+++ Node-Learning/src/server.ts
@@ -1,8 +1,23 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-  console.log(req);
-});
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    // console.log(req.url); '/' , "/user","/products
+    // console.log(req.method); "GET" ,"POST" , "DELETE"
+    const url = req.url;
+    const method = req.method;
+    if (url === "/" && method === "GET") {
+      //console.log("This is root route");
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "This is root route" }));
+    } else if (url?.startsWith("/products")) {
+      res.end(JSON.stringify({ message: "This is product route" }));
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Route not found" }));
+    }
+  },
+);
 
 server.listen(5000, () => {
   console.log("Server is running on port 5000");
