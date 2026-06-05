6b25e41b19b8d6e5e60cdf38852c44e519c85500
ArafatSany7 <human.sany7@gmail.com>
2026-06-05 12:00:00 +0600

    Commit

--- TypeScript-Learning/src/class.ts
+++ TypeScript-Learning/src/class.ts
@@ -37,17 +37,15 @@ class Animal {
   }
 }
 
-const dog = new Animal("Dogesh", "Dog", "gheo gheo");
+const dog = new Animal("Dogesh ", "Dog", "gheo gheo");
 const cat = new Animal("Billu", "Bilai", "mew mew");
 
 console.log(dog.name);
 console.log(cat.sound);
 cat.makesound();
 
-
 function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((num) => num % 2 === 0);
 }
 console.log("RUNNING...");
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
-
