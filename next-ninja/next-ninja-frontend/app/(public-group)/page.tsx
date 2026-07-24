df1a4df70b2415a809562e5928b6565e5c2ebad2
ArafatSany7 <human.sany7@gmail.com>
2026-07-24 23:28:00 +0600

    28-7

--- next-ninja/next-ninja-frontend/app/(public-group)/page.tsx
+++ next-ninja/next-ninja-frontend/app/(public-group)/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+
 export default function HomePage() {
   console.log('Root Route');
   return (
     <div>
       Hello next-ninja-frontend
+      <Button
+        size="lg" variant="destructive"
+      >
+        Click me
+      </Button>
     </div>
   );
 }
--- next-ninja/next-ninja-frontend/app/layout.tsx
+++ next-ninja/next-ninja-frontend/app/layout.tsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import { Oxanium, Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geistHeading = Geist({subsets:['latin'],variable:'--font-heading'});
+
+const oxanium = Oxanium({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -8,9 +15,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={` h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", oxanium.variable, geistHeading.variable)}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         {/*navbar*/}
         {children}
         {/*footer*/}
